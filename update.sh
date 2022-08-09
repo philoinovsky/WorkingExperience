dbf9b5b7d5b3ba0f8fbd4a661bbdbadc86509bc1
philoinovsky <1129410550@qq.com>
2022-08-09 16:09:22 +0000

    commit whatever

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-cat \#/README.md > README.md
-echo '```' >> README.md
-tree >> README.md
-echo '```' >> README.md
-git add .
-read time <<< $(echo $(date) | awk '{print $2, $3, $6}')
-git commit -m "updated at ""$time"
-git push origin master
